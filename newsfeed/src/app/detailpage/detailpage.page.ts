dd52841bef6926927a7d49b079ad1c0fade4b395
ravikhb <pingravikumar@gmail.com>
2019-11-18 12:03:17 +0530

    detailpage added to show selected content

--- newsfeed/src/app/detailpage/detailpage.page.ts
+++ newsfeed/src/app/detailpage/detailpage.page.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Article } from '../network/articles';
 
 @Component({
   selector: 'app-detailpage',
   templateUrl: './detailpage.page.html',
   styleUrls: ['./detailpage.page.scss'],
 })
-export class DetailpagePage implements OnInit {
 
-  constructor() { }
+export class DetailpagePage implements OnInit{
+  
+  author: string;
+  content: string;
+  description: string;
+  publishedAt: string;
+  title: string;
+  url: string;
+  urlToImage: string;
 
-  ngOnInit() {
+  @Input() selectedItem: Article;
+  itemSelected:any;
+
+  constructor(private router:Router) { 
+    this.itemSelected = this.router.getCurrentNavigation().extras.state;
+   }
+
+  ngOnInit(): void {
+    for( const key in this.itemSelected) {
+      this.description = this.itemSelected['item'].description;
+      this.title = this.itemSelected['item'].title;
+      this.urlToImage = this.itemSelected['item'].urlToImage;
+      this.content = this.itemSelected['item'].content;
+      this.author = this.itemSelected['item'].author;
+    }
   }
 
-}
+  }
+
